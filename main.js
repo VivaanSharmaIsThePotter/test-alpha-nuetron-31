a79328dabdae37ee11bacd3b0973fc5651989182
VivaanSharmaIsThePotter <73700041+VivaanSharmaIsThePotter@users.noreply.github.com>
2021-06-28 17:54:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+objects = [];
+status = "";
 video = "";
 
 function preload() {
-    video = createVideo('video.mp4');
+    video = createVideo("cool_video.mp4");
     video.hide();
 }
 
@@ -12,6 +14,28 @@ function setup() {
 
 function draw() {
     image(video, 0, 0, 480, 380);
+    if (status != "") {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are = " + objects.length;
+            
+            fill("#ff0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#ff0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
 
 function start() {
